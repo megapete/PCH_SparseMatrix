53d018f76da9d50e37947190e673f71fe3834cc8
Peter Huber <peter@huberistech.com>
2018-04-14 12:50:51 -0400

    Initial Commit

---
+++ PCH_SparseMatrix/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PCH_SparseMatrix
+//
+//  Created by PeterCoolAssHuber on 2018-04-14.
+//  Copyright © 2018 Peter Huber. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
