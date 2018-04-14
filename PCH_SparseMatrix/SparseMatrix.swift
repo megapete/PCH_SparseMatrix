161496bd6a162571ea125844e1a62f77e3299a2b
Peter Huber <peter@huberistech.com>
2018-04-14 18:49:48 -0400

    Implemented creation routines for Sparse and Dense matrices

--- PCH_SparseMatrix/SparseMatrix.swift
+++ PCH_SparseMatrix/SparseMatrix.swift
@@ -8,14 +8,43 @@
 
 // This is a wrapper around Apple's sparse matrix routines. The reason for taking this on is that Apple does not offer Complex precision, which we need for a number of programs. Also, the Apple routines for creating a memory structure for Sparse matrices assumes that the entries in the matrix are already known (which makes it kinda useless if you ask me). Our strategy here is one we've used before where we store the numbers in a dictionary where the keys are instances of a special index class and the values are the Complex values (duh). We will use the Complex Matrix Theory that we developed some time ago where any complex number is converted into a 2x2 matrix with the real number repeated on the diagonal, the negative of the imaginary part on the first row and the unmodified impaginary part on the second row.
 
+// Note: All row and column indices are 0-based
+
 import Foundation
 import Accelerate
 
-class PCH_SparseMatrix
+class PCH_SparseMatrix:CustomStringConvertible
 {
     // This is the number of rows and cols in the "virtual" Complex matrix (which does not really exist, since we actually store each complex as a 2x2 matrix)
     let rows:Int
     let cols:Int
+    
+    /// As simple description function to display matrices with 'print' (the output is quite ugly and not nicely formatted)
+    var description:String
+    {
+        var result = ""
+        
+        for i in 0..<self.rows
+        {
+            result += "\n| "
+            
+            for j in 0..<self.cols
+            {
+                if (j == self.cols - 1)
+                {
+                    let value:Complex = self[i,j]
+                    result += "\(value) |"
+                }
+                else
+                {
+                    let value:Complex = self[i,j]
+                    result += "\(value)   "
+                }
+            }
+        }
+        
+        return result
+    }
     
     struct SparseKey:Hashable
     {
@@ -71,15 +100,32 @@ class PCH_SparseMatrix
                 ALog("Illegal index")
             }
             
-            let key = SparseKey(row: row, col: column)
+            let realKey1 = SparseKey(row: row * 2, col: column * 2)
+            let realKey2 = SparseKey(row: row * 2 + 1, col: column * 2 + 1)
+            let imagKey1 = SparseKey(row: row * 2, col: column * 2 + 1) // negative imaginary term
+            let imagKey2 = SparseKey(row: row * 2 + 1, col: column * 2)
             
-            if newValue == Complex(real: 0.0)
+            if newValue.real == 0.0
             {
-                self.matrix.removeValue(forKey: key)
+                self.matrix.removeValue(forKey: realKey1)
+                self.matrix.removeValue(forKey: realKey2)
             }
             else
             {
-                self.matrix[key] = newValue
+                self.matrix[realKey1] = newValue.real
+                self.matrix[realKey2] = newValue.real
+            }
+            
+            if newValue.imag == 0.0
+            {
+                
+                self.matrix.removeValue(forKey: imagKey1)
+                self.matrix.removeValue(forKey: imagKey2)
+            }
+            else
+            {
+                self.matrix[imagKey1] = -newValue.imag
+                self.matrix[imagKey2] = newValue.imag
             }
         }
     }
@@ -94,31 +140,62 @@ class PCH_SparseMatrix
     {
         var rowIndices:[Int32] = Array(repeating: -1, count: self.matrix.count * 2)
         var values:[Double] = Array(repeating: Double.greatestFiniteMagnitude, count: self.matrix.count * 2)
-        var columnStarts:[Int32] = Array(repeating: -1, count: self.cols * 2 + 1)
+        var columnStarts:[Int] = Array(repeating: -1, count: self.cols * 2 + 1)
         
         // We assume that the user has done SOME work and that every column has at least one entry in it...
         var lastColumnStart = 0
-        columnStarts[0] = 0
+        columnStarts[0] = lastColumnStart
         
-        
-        for (key, value) in self.matrix
+        for column in 0..<self.cols
         {
+            var numRowValues:Int32 = 0
+            for row in 0..<self.rows
+            {
+                if let value = self.matrix[SparseKey(row: row, col: column)]
+                {
+                    if value != 0.0
+                    {
+                        rowIndices.append(Int32(row))
+                        values.append(value)
+                        numRowValues += 1
+                    }
+                }
+            }
             
+            lastColumnStart += Int(numRowValues)
+            columnStarts[column + 1] = lastColumnStart
         }
+        
+        let sparseStruct = SparseMatrixStructure(rowCount: Int32(self.rows * 2), columnCount: Int32(self.cols * 2), columnStarts: &columnStarts, rowIndices: &rowIndices, attributes: SparseAttributes_t(), blockSize: 1)
+        
+        return SparseMatrix_Double(structure: sparseStruct, data: &values)
     }
     
-    func NumberOfEntriesInColumn(_ col:Int) -> Int
+    static func CreateDenseMatrixForComplexVector(values:[Complex]) -> DenseMatrix_Double
     {
-        var result = 0
-        for (key, value) in self.matrix
+        let rowCount = values.count
+        let data = UnsafeMutablePointer<Double>.allocate(capacity: rowCount * 4)
+        
+        var i = 0
+        for nextValue in values
         {
-            if key.col == col
-            {
-                result += 1
-            }
+            data[i] = nextValue.real
+            data[i + 1] = nextValue.imag
+            data[i + rowCount] = -nextValue.imag
+            data[i + 1 + rowCount] = nextValue.real
+            
+            i += 1
         }
         
+        let result = DenseMatrix_Double(rowCount: Int32(rowCount * 2), columnCount: 2, columnStride: 1, attributes: SparseAttributes_t(), data: data)
+        
         return result
+    }
+    
+    // See the Apple documentation under Sparse to see how to create a DenseMatrix
+    func SolveWithB(_ B:DenseMatrix_Double) -> DenseMatrix_Double
+    {
+        
     }
     
 }
